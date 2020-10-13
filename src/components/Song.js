0b21ade1e9c8085fbf11e93c059ae16e4ff2a0e5
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-10-13 19:48:59 +0100

    song links and images added

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Song(props){
     return(
-        <div>
-            <li>{props.song["im:name"].label} by {props.song["im:artist"].label} </li>
+        <div className='song-list'>
+            <li><a href={props.song.link[1].attributes.href}>{props.song["im:name"].label}</a> by {props.song["im:artist"].label}
+            <p><img src={props.song['im:image'][0].label}/></p></li>
         </div>
     )
 }
